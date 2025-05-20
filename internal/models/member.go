4c583c7dcf3ebbf820732ba5ebb649ef9298cebc
Jide Tireni <jtirenipraise@gmail.com>
2025-05-20 17:54:45 +0100

    feat: add MemberResponse struct and NewMemberResponse function for member data handling

--- internal/models/member.go
+++ internal/models/member.go
@@ -1,12 +1,35 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Member struct {
 	gorm.Model
 	UserID      uint   `gorm:"unique"`
 	Name        string `gorm:"not null"`
 	ContactInfo string `gorm:"not null"`
-	User        User
-	Savings     []Savings
+}
+
+type MemberResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	// DeletedAt   string `json:"deleted_at"`
+	Name        string `json:"name"`
+	ContactInfo string `json:"contact_info"`
+	UserID      uint   `json:"user_id"`
+}
+
+func NewMemberResponse(member *Member) MemberResponse {
+	return MemberResponse{
+		ID:          member.ID,
+		CreatedAt:   member.CreatedAt,
+		UpdatedAt:   member.UpdatedAt,
+		Name:        member.Name,
+		ContactInfo: member.ContactInfo,
+		UserID:      member.UserID,
+	}
 }
