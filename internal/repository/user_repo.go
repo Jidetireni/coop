672a382057da20e0a2b259575742451c811a2753
Jide Tireni <jtirenipraise@gmail.com>
2025-05-20 17:55:45 +0100

    refactor: update gormUserRepository methods to return detailed responses and improve error handling

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -6,23 +6,37 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserRepository struct {
+type gormUserRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) *UserRepository {
-	return &UserRepository{db: db}
+func NewUserRepository(db *gorm.DB) *gormUserRepository {
+	return &gormUserRepository{db: db}
 }
 
-func (r *UserRepository) CreateUser(user *models.User) error {
-	return r.db.Create(user).Error
+func (r *gormUserRepository) CreateUser(user *models.User) (*models.User, string, error) {
+
+	if err := r.db.Create(&user).Error; err != nil {
+		return nil, "failed to create user", err
+	}
+	return user, "user created successfully", nil
+
 }
 
-func (r *UserRepository) FindUserByEmail(email string) (*models.User, error) {
+func (r *gormUserRepository) FindUserByEmail(email string) (*models.User, string, error) {
 	var user models.User
 	err := r.db.Where("email = ?", email).First(&user).Error
 	if err != nil {
-		return nil, err
+		return nil, "failed to find user", err
 	}
-	return &user, nil
+	return &user, "success", nil
+}
+
+func (r *gormUserRepository) UpdateUser(user *models.User, role string) (*models.User, string, error) {
+
+	if err := r.db.Model(&user).Updates(role).Error; err != nil {
+		return nil, "failed to update user", err
+	}
+
+	return user, "user updated successfully", nil
 }
