3994c9d228114d936e87611b53f3d415f6676b91
Jide Tireni <jtirenipraise@gmail.com>
2025-05-20 17:55:14 +0100

    feat: add UserResponse struct and NewUserResponse function for user data handling

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -9,4 +11,23 @@ type User struct {
 	Email    string `gorm:"unique"`
 	Password string `gorm:"not null"`
 	Role     string `gorm:"not null"`
+}
+
+type UserResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+}
+
+func NewUserResponse(user *User) UserResponse {
+	return UserResponse{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+		Role:      user.Role,
+	}
+
 }
