c803f37f944624ff8b10e10f363fec773c7255f9
Jide Tireni <jtirenipraise@gmail.com>
2025-05-20 17:54:53 +0100

    refactor: reorganize imports and enhance SavingsResponse and SavingTransactionResponse structures

--- internal/models/savings.go
+++ internal/models/savings.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Savings struct {
 	gorm.Model
@@ -9,7 +13,6 @@ type Savings struct {
 	Balance      int  `gorm:"not null"`
 	AmountToSave int  `gorm:"not null"`
 	Description  string
-	Member       Member
 }
 
 type SavingTransaction struct {
@@ -20,4 +23,48 @@ type SavingTransaction struct {
 	Description string
 	// TransactionDate time.Time
 	Savings Savings
+}
+
+type SavingsResponse struct {
+	ID           uint      `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Balance      int       `json:"balance"`
+	AmountToSave int       `json:"amount_to_save"`
+	Description  string    `json:"description"`
+	MemberID     uint      `json:"member_id"`
+}
+
+func NewSavingsResponse(savings *Savings) SavingsResponse {
+	return SavingsResponse{
+		ID:           savings.ID,
+		CreatedAt:    savings.CreatedAt,
+		UpdatedAt:    savings.UpdatedAt,
+		Balance:      savings.Balance,
+		AmountToSave: savings.AmountToSave,
+		Description:  savings.Description,
+		MemberID:     savings.MemberID,
+	}
+}
+
+type SavingTransactionResponse struct {
+	ID          uint      `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Amount      int       `json:"amount"`
+	Description string    `json:"description"`
+	MemberID    uint      `json:"member_id"`
+	SavingsID   uint      `json:"savings_id"`
+}
+
+func NewSavingTransactionResponse(transaction *SavingTransaction) SavingTransactionResponse {
+	return SavingTransactionResponse{
+		ID:          transaction.ID,
+		CreatedAt:   transaction.CreatedAt,
+		UpdatedAt:   transaction.UpdatedAt,
+		Amount:      transaction.Amount,
+		Description: transaction.Description,
+		MemberID:    transaction.MemberID,
+		SavingsID:   transaction.SavingsID,
+	}
 }
