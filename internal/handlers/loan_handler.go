46bdb2cbc1213b404c7c9262be9ea7be239ccf94
Jidetireni <ltirenipraise@gmail.com>
2025-05-21 21:38:36 +0000

    fix: refactor loan handler to implement GetLoanStatus and TrackLoanApproval methods with improved loan history management

--- internal/handlers/loan_handler.go
+++ internal/handlers/loan_handler.go
@@ -31,6 +31,7 @@ func NewLoanHandler(loanRepo repository.LoanRepository, memberRepo repository.Me
 
 type LoanService interface {
 	ApplyLoan(c *gin.Context)
+	GetLoanStatus(c *gin.Context)
 	TrackLoanApproval(c *gin.Context)
 }
 
@@ -63,12 +64,14 @@ func (l *LoanHandler) ApplyLoan(c *gin.Context) {
 		return
 	}
 
+	// Fetch the member associated with the authenticated user
 	member, msg, err := l.memberRepo.FetchMemberByUserID(authUser.ID)
 	if err != nil {
 		utils.RespondWithError(c, http.StatusInternalServerError, msg, err)
 		return
 	}
 
+	// Calculate the interest rate based on the loan type and term
 	calculatedInterestRate := models.GetInterestRate(reqBody.Type, reqBody.LoanTermMonths)
 	if calculatedInterestRate == 0 {
 		utils.RespondWithError(c, http.StatusBadRequest, "invalid loan type or term", nil)
@@ -92,29 +95,37 @@ func (l *LoanHandler) ApplyLoan(c *gin.Context) {
 		Description:          reqBody.Description,
 		MemberID:             member.ID,
 		InterestRate:         calculatedInterestRate,
-		Status:               "pending",
+		Status:               models.LoanStatusPending,
 		Type:                 reqBody.Type,
 		LoanTermMonths:       reqBody.LoanTermMonths,
 		TotalRepayableAmount: totalRepayableAmount,
 		InstallmentAmount:    installmentAmount,
+		IsActive:             false,
 	}
 
-	// Call the repository method to create a new loan
-	createdLoan, msg, err := l.repo.CreateLoanRequestObject(&loan)
+	tempInitialHistory := models.LoanHistory{
+		Status:    models.LoanStatusPending,
+		ChangedBy: member.ID,
+		Remarks:   "Loan application submitted",
+	}
+
+	createdLoan, returnedHistory, msg, err := l.repo.CreateLoanWithInitialHistory(&loan, &tempInitialHistory)
 	if err != nil {
 		utils.RespondWithError(c, http.StatusInternalServerError, msg, err)
 		return
 	}
 
 	loanResponse := models.NewLoanResponse(createdLoan)
+	historyResponse := models.NewLoanHistoryResponse(returnedHistory)
 
 	utils.SuccessResponse(c, http.StatusCreated, "loan application submitted successfully", "data", gin.H{
-		"loan": loanResponse,
+		"loan":           loanResponse,
+		"intial_history": historyResponse,
 	})
 
 }
 
-func (l *LoanHandler) TrackLoanApproval(c *gin.Context) {
+func (l *LoanHandler) GetLoanStatus(c *gin.Context) {
 	loanID := c.Param("loan_id")
 	if loanID == "" {
 		utils.RespondWithError(c, http.StatusBadRequest, "loan ID is required", nil)
@@ -153,4 +164,56 @@ func (l *LoanHandler) TrackLoanApproval(c *gin.Context) {
 		"loan": loanResponse,
 	})
 
+}
+
+func (l LoanHandler) TrackLoanApproval(c *gin.Context) {
+	loanID := c.Param("loan_id")
+	if loanID == "" {
+		utils.RespondWithError(c, http.StatusBadRequest, "loan ID is required", nil)
+		return
+	}
+
+	authUser, ok := getAuthUser(c)
+	if !ok {
+		utils.RespondWithError(c, http.StatusUnauthorized, "unauthenticated user", nil)
+		return
+	}
+
+	loan, msg, err := l.repo.GetLoanByID(loanID)
+	if err != nil {
+		if loan == nil {
+			utils.RespondWithError(c, http.StatusNotFound, msg, err)
+		} else {
+			utils.RespondWithError(c, http.StatusInternalServerError, "error fetching loan: "+msg, err)
+		}
+		return
+	}
+
+	member, msg, err := l.memberRepo.FetchMemberByUserID(authUser.ID)
+	if err != nil {
+		utils.RespondWithError(c, http.StatusInternalServerError, msg, err)
+		return
+	}
+
+	if loan.MemberID != member.ID {
+		utils.RespondWithError(c, http.StatusForbidden, "you are not authorized to view this loan", nil)
+		return
+	}
+
+	loanHistories, msg, err := l.repo.GetLoanHistoryByID(loanID)
+	if err != nil {
+		utils.RespondWithError(c, http.StatusInternalServerError, msg, err)
+		return
+	}
+
+	historiesResponse := make([]models.LoanHistoryResponse, len(loanHistories))
+	for i, history := range loanHistories {
+		currentHistory := history
+		historiesResponse[i] = models.NewLoanHistoryResponse(&currentHistory)
+	}
+
+	utils.SuccessResponse(c, http.StatusOK, "loan approval status fetched successfully", "data", gin.H{
+		"loan":            models.NewLoanResponse(loan),
+		"approval_status": historiesResponse,
+	})
 }
